498535b1f000fd157a4447ff9e991816119f5ba3
ldsantos0911 <louis.santos.0911@gmail.com>
2018-03-04 00:48:11 -0800

    Added tentative minimax

--- player.hpp
+++ player.hpp
@@ -7,6 +7,21 @@
 #include "board.hpp"
 using namespace std;
 
+class State {
+
+public:
+    State(Board *board, Move* move)
+    {
+        this->board = board;
+        this->move = move;
+    }
+    ~State()
+    {
+    }
+    Board *board;
+    Move* move;
+};
+
 class Player {
 
 public:
@@ -14,14 +29,17 @@ public:
     ~Player();
 
     Move *doMove(Move *opponentsMove, int msLeft);
-    vector<Move*> possMoves();
+    vector<Move*> possMoves(Board board, Side side);
     Move* best(vector<Move*> poss);
+    Move* mini_max();
+    Move *findOppMove(Board board);
 
     // Flag to tell if the player is running within the test_minimax context
-    bool testingMinimax;
+    bool testingMinimax = true;
     Side opponent;
     Board currBoard;
     Side side;
+    vector<State> states;
 };
 
 #endif
